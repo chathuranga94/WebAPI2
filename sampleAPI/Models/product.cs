b1de381d983315ab18ff80637f9e8b2187a3b0b2
chathuranga94 <chathuranga94@live.com>
2016-09-13 07:02:27 +0530

    html page added

--- sampleAPI/Models/product.cs
+++ sampleAPI/Models/product.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace sampleAPI.Models
 {
-    public class product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
